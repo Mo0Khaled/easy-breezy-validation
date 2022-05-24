84df5f839193f5eebcfb0d575e2d911530f32478
Mo0Khaled <mohamedkx77@gmail.com>
2022-05-24 05:15:06 -0500

    document the date of birth

--- lib/text/date_of_birth.dart
+++ lib/text/date_of_birth.dart
@@ -1,6 +1,15 @@
 part of 'package:validator/validator.dart';
 
-
+/// validate the name [dateOfBirth]
+///
+/// it takes dayOfBirth of type string to validate
+/// it must contains year at the beginning day at the end
+///
+/// the date should be formatted like( [year] (symbol) [month] (symbol) [day] )
+/// ``` dart
+/// isDateOfBirth("2000/05/21") == true
+/// isDateOfBirth("1999/5/3") == false
+/// ```
 bool isDateOfBirth(String date) {
 
   final dateRegex = RegExp(r'^(\d{4})[-\.\/:](0[1-9]|1[0-2])[-\.\/:](0[1-9]|[12][0-9]|3[01])$');
